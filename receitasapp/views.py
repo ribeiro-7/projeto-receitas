227c7f768e4bc7bbc81d257561a1bafcf2627c74
ribeirin-7 <arthurr366@gmail.com>
2025-06-24 15:28:49 -0300

    Implementando view de receitas

--- receitasapp/views.py
+++ receitasapp/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'receitas/pages/home.html')
+
+def receitas(request, pk):
+    return render(
+        request, 
+        'receitas/pages/receita-view.html'
+    )
