3d0a925c4e9e10439af4c69d1d7a7778ff5585d4
ribeirin-7 <arthurr366@gmail.com>
2025-06-25 15:32:26 -0300

    Testando as views com dados aleatorios

--- receitasapp/views.py
+++ receitasapp/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from utils.receitas.factory import make_recipe
 
 def home(request):
-    return render(request, 'receitas/pages/home.html')
+    return render(
+        request, 
+        'receitas/pages/home.html',
+        context={
+            'receitas': [make_recipe() for _ in range(10)],
+        }
+    )
 
 def receitas(request, pk):
     return render(
         request, 
-        'receitas/pages/receita-view.html'
+        'receitas/pages/receita-view.html',
+        context={
+            'receita': make_recipe()
+        }
     )
