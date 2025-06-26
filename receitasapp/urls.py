36940654d0dde5b11176d191420b906c3055f1ee
ribeirin-7 <arthurr366@gmail.com>
2025-06-26 13:19:57 -0300

    Implementando namespace para as urls com 'app_name'

--- receitasapp/urls.py
+++ receitasapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from receitasapp import views
 
+app_name = 'receitas'
+
 urlpatterns = [
-    path('', views.home),
-    path('receita/<int:pk>/', views.receita),
+    path('', views.home, name='home'),
+    path('receita/<int:pk>/', views.receita, name='receita'),
 ]
