12dc411606fbdb965abac92b2122594b7a069db3
ribeirin-7 <arthurr366@gmail.com>
2025-06-27 14:03:59 -0300

    Alterando caminho de imagem e adicionando __str__ para admin

--- receitasapp/models.py
+++ receitasapp/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 class Receita(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -17,6 +20,9 @@ class Receita(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='receita/cover/%Y/%m/%d/')
+    cover = models.ImageField(upload_to='receitasapp/cover/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
