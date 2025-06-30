a84d086fa00a17b7449b84cd8fb83a7932aedf53
ribeirin-7 <arthurr366@gmail.com>
2025-06-30 12:23:31 -0300

    Criando url para view de category

--- receitasapp/urls.py
+++ receitasapp/urls.py
@@ -5,5 +5,6 @@ app_name = 'receitas'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('receita/categoria/<int:category_pk>/', views.categoria, name='categoria'),
     path('receita/<int:pk>/', views.receita, name='receita'),
 ]
