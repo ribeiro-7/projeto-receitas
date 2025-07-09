9fb262ea3aef5b95cb406439eb8f8eb82a3f561d
ribeirin-7 <arthurr366@gmail.com>
2025-07-09 11:44:09 -0300

    Alterando a variavel de id para receita_id para melhor entendimento

--- receitasapp/urls.py
+++ receitasapp/urls.py
@@ -6,5 +6,5 @@ app_name = 'receitas'
 urlpatterns = [
     path('', views.home, name='home'),
     path('receita/categoria/<int:category_id>/', views.categoria, name='categoria'),
-    path('receita/<int:id>/', views.receita, name='receita'),
+    path('receita/<int:receita_id>/', views.receita, name='receita'),
 ]
